2102133dc975d33c15b19e2d050cff0f80e8687c
Albert Ramirez <albert@cloudcamp.la>
2023-02-23 01:08:56 +0000

    Ejercicios python clase 1

---
+++ ejercicios_python/cafeteria.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+print("Hola, bienvenido a la cafeteria.")
+nombre = input("Cual es tu nombre? \n")
+
+
+menu = "Cafe, te y agua"
+orden = input("Hola, " + nombre + " que desea ordenar de nuestro menu? \n" + menu + "\n")
+
+print(nombre + " tu " + orden + " estara en un momento. ")
